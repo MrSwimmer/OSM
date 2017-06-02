d3761d29e1d375ab7acfab8d1b83316c14db3b30
MrSwimmer <mrswimmerlab@gmail.com>
2017-06-02 21:24:20 +0700

    Релиз

--- app/src/main/java/com/bignerdranch/android/osm/NoteSettings.java
+++ app/src/main/java/com/bignerdranch/android/osm/NoteSettings.java
@@ -4,8 +4,10 @@ package com.bignerdranch.android.osm;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +16,8 @@ import com.bignerdranch.android.osm.database.ExportImportDB;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Севастьян on 08.03.2017.
@@ -21,6 +25,9 @@ import com.google.android.gms.ads.MobileAds;
 
 public class NoteSettings extends AppCompatActivity {
     private static final String DIALOG_DATE = "DialogDate";
+    FirebaseUser user;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     private TextView mButton;
     private TextView mMark;
     private TextView mEnterRegButton;
@@ -28,9 +35,39 @@ public class NoteSettings extends AppCompatActivity {
     private TextView mVer;
     private AdView mAdView;
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.settings_note);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    String email = user.getEmail();
+                    Log.i("LOG", email);
+                    mEnterRegButton.setText("Вход выполнен!\n" + email);
+                    // User is signed in
+                } else {
+                    // User is signed out
+                }
+                // ...
+            }
+        };
 //        mAuthListener = new FirebaseAuth.AuthStateListener() {
 //            @Override
 //            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -47,7 +84,7 @@ public class NoteSettings extends AppCompatActivity {
 //                // ...
 //            }
 
-        setContentView(R.layout.settings_note);
+
         MobileAds.initialize(getApplicationContext(), "ca-app-pub-3781095842244998/9275756860");
         mAdView = (AdView) findViewById(R.id.adViewtwo);
         AdRequest adRequest = new AdRequest.Builder().build();
@@ -99,6 +136,5 @@ public class NoteSettings extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
-
 
 }
--- app/src/main/java/com/bignerdranch/android/osm/SplashScreenActivity.java
+++ app/src/main/java/com/bignerdranch/android/osm/SplashScreenActivity.java
@@ -2,23 +2,74 @@ package com.bignerdranch.android.osm;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class SplashScreenActivity extends Activity {
-
     // Время в милесекундах, в течение которого будет отображаться Splash Screen
     private final int SPLASH_DISPLAY_LENGTH = 2000;
+    StorageReference riversRef;
+    FirebaseUser user;
     private String[] tip = new String[3];
     private TextView mTextView;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private StorageReference mStorageRef;
 
     @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    @Override
     public void onCreate(Bundle savedInstanceState) {
+
         tip[0] = "Подсказка: при создании записи для удобства вы можете воспользоваться встроенным секундомером";
         tip[1] = "Подсказка: для удобства поиска нужной записи воспользуйтесь сортировкой или поиском по дате";
         tip[2] = "Подсказка: теперь вы можете уведомлять своего тренера о результате пробы, воспользовавшись функцией - отправить результат";
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+
+                    mStorageRef = FirebaseStorage.getInstance().getReference();
+                    String Name = user.getEmail();
+                    riversRef = mStorageRef.child(Name + "/noteBase.db");
+                    upload();
+                    // User is signed in
+                } else {
+                    // User is signed out
+                }
+                // ...
+            }
+        };
         setContentView(R.layout.splash_screen);
         mTextView = (TextView) findViewById(R.id.splash_tip);
         int max = 2;
@@ -34,4 +85,26 @@ public class SplashScreenActivity extends Activity {
         }, SPLASH_DISPLAY_LENGTH);
     }
 
+    public void upload() {
+        final Uri file = Uri.fromFile(new File(Environment.getDataDirectory(), "//data//" + "com.bignerdranch.android.osm"
+                + "//databases//" + "noteBase.db"));
+        Toast.makeText(SplashScreenActivity.this, "Идет загрузка", Toast.LENGTH_SHORT).show();
+        riversRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Toast.makeText(SplashScreenActivity.this, "Загрузка завершена успешно!", Toast.LENGTH_SHORT).show();
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        Toast.makeText(SplashScreenActivity.this, "Ошибка!", Toast.LENGTH_SHORT).show();
+                        // ...
+                    }
+                });
+    }
 }
